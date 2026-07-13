37378a308d33f7878cb1edf4202dd4e9dcd271f1
Kieu Kien Quoc <kieukienquocvn@gmail.com>
2026-07-14 01:40:31 +0700

    fix(test): use @MockBean for Elasticsearch repo instead of invalid exclude param

--- server/src/test/java/com/rrms/rrms/RrmsApplicationTests.java
+++ server/src/test/java/com/rrms/rrms/RrmsApplicationTests.java
@@ -1,20 +1,19 @@
 package com.rrms.rrms;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchClientAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.TestPropertySource;
 
+import com.rrms.rrms.repositories.BulletinBoardElasticsearchRepository;
+
 @TestPropertySource(locations = "classpath:test.properties")
-@SpringBootTest(
-        exclude = {
-            ElasticsearchClientAutoConfiguration.class,
-            ElasticsearchRepositoriesAutoConfiguration.class,
-            ElasticsearchDataAutoConfiguration.class
-        })
+@SpringBootTest
 class RrmsApplicationTests {
+
+    @MockBean
+    BulletinBoardElasticsearchRepository bulletinBoardElasticsearchRepository;
+
     @Test
     void contextLoads() {}
 }
